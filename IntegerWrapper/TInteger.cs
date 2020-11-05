3f2c6b74aaea4ae68d33c696396c87715ad2df95
panke <rene.panke@ser.de>
2020-11-05 15:06:02 +0100

    Initial commit.

---
+++ IntegerWrapper/TInteger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace IntegerWrapper
+{
+    public class TInteger
+    {
+        #region Eigenschaften
+        public int IntValue { get; set; }
+        #endregion
+
+        #region Konstruktoren
+        public TInteger()
+        {
+            IntValue = 0;
+        }
+
+        public TInteger(int value)
+        {
+            IntValue = value;
+        }
+        #endregion
+
+        #region Worker
+        public TInteger Addition(TInteger value)
+        {
+            TInteger result = new TInteger();
+            result.IntValue = IntValue + value.IntValue;
+            return result;
+        }
+
+        public TInteger Subtraktion(TInteger value)
+        {
+            TInteger result = new TInteger();
+            result.IntValue = IntValue - value.IntValue;
+            return result;
+        }
+
+        public TInteger Multiplikation(TInteger value)
+        {
+            TInteger result = new TInteger();
+            result.IntValue = IntValue * value.IntValue;
+            return result;
+        }
+
+        public TInteger Division(TInteger value)
+        {
+            TInteger result = new TInteger();
+            result.IntValue = IntValue / value.IntValue;
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ IntegerWrapper/main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntegerWrapper
+{
+    class main
+    {
+        public static void Main()
+        {
+            TInteger firstInt = new TInteger(5);
+            TInteger secondInt = new TInteger(4);
+
+            Console.WriteLine(firstInt.Addition(secondInt).IntValue);
+            Console.WriteLine(firstInt.Subtraktion(secondInt).IntValue);
+            Console.WriteLine(firstInt.Multiplikation(secondInt).IntValue);
+            Console.WriteLine(firstInt.Division(secondInt).IntValue);
+            Console.ReadKey();
+        }
+    }
+}
